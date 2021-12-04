1fd5c07637a979490345f28f82a84070fe1c88b7
JayCanuck <robitaille88@gmail.com>
2021-12-03 17:40:44 -0800

    Added N64 and PSP filters for rg351p devices of known compatible games.

---
+++ filters/rg351p/n64-compat.js
@@ -0,0 +1,52 @@
+/*
+	A basic list of N64 games confirmed compatible with rg351p.
+	Sourced from an Anbernic Facebook group.
+	Not exhaustive list and open to additions based off feedback.
+*/
+
+module.exports = {
+	include: [
+		{ id: '5534', system: 'Nintendo 64' }, // Aidyn Chronicles : The First Mage
+		{ id: '5671', system: 'Nintendo 64' }, // Asteroids Hyper 64
+		{ id: '5405', system: 'Nintendo 64' }, // Banjo-Kazooie
+		{ id: '5411', system: 'Nintendo 64' }, // Banjo-Tooie
+		{ id: '71667', system: 'Nintendo 64' }, // Batman Beyond : Return of the Joker
+		{ id: '5600', system: 'Nintendo 64' }, // Bio F.R.E.A.K.S.
+		{ id: '5516', system: 'Nintendo 64' }, // Bomberman 64 : The Second Attack!
+		{ id: '5519', system: 'Nintendo 64' }, // Bomberman Hero
+		{ id: '5525', system: 'Nintendo 64' }, // Buck Bumble
+		{ id: '5626', system: 'Nintendo 64' }, // Bust-A-Move '99
+		{ id: '5580', system: 'Nintendo 64' }, // Bust-A-Move 2: Arcade Edition
+		{ id: '5414', system: 'Nintendo 64' }, // Castlevania
+		{ id: '5428', system: 'Nintendo 64' }, // Castlevania : Legacy of Darkness
+		{ id: '5460', system: 'Nintendo 64' }, // Disney's Tarzan
+		{ id: '5404', system: 'Nintendo 64' }, // Donkey Kong 64
+		{ id: '5446', system: 'Nintendo 64' }, // Doom 64
+		{ id: '5569', system: 'Nintendo 64' }, // Dual Heroes
+		{ id: '5456', system: 'Nintendo 64' }, // Duke Nukem : Zero Hour
+		{ id: '5615', system: 'Nintendo 64' }, // Fighter Destiny 2
+		{ id: '5565', system: 'Nintendo 64' }, // Fighters Destiny
+		{ id: '5633', system: 'Nintendo 64' }, // Flying Dragon
+		{ id: '5504', system: 'Nintendo 64' }, // Gex 3 : Deep Cover Gecko
+		{ id: '5491', system: 'Nintendo 64' }, // Gex 64 : Enter the Gecko
+		{ id: '5438', system: 'Nintendo 64' }, // Kirby 64 : The Crystal Shards
+		{ id: '5394', system: 'Nintendo 64' }, // The Legend of Zelda : Majora's Mask
+		{ id: '5395', system: 'Nintendo 64' }, // The Legend of Zelda : Ocarina of Time
+		{ id: '5510', system: 'Nintendo 64' }, // Mace : The Dark Age
+		{ id: '5401', system: 'Nintendo 64' }, // Mario Kart 64
+		{ id: '5455', system: 'Nintendo 64' }, // Mega Man 64
+		{ id: '5457', system: 'Nintendo 64' }, // Mortal Kombat 4
+		{ id: '5451', system: 'Nintendo 64' }, // Mortal Kombat Mythologies : Sub-Zero
+		{ id: '59532', system: 'Nintendo 64' }, // O.D.T. (Proto.)
+		{ id: '5545', system: 'Nintendo 64' }, // Paperboy
+		{ id: '5403', system: 'Nintendo 64' }, // Perfect Dark
+		{ id: '59520', system: 'Nintendo 64' }, // Quest 64
+		{ id: '5535', system: 'Nintendo 64' }, // Rampage 2 : Universal Tour
+		{ id: '5658', system: 'Nintendo 64' }, // Robotron 64
+		{ id: '5553', system: 'Nintendo 64' }, // Scooby-Doo! - Classic Creep Capers
+		{ id: '5396', system: 'Nintendo 64' }, // Super Mario 64
+		{ id: '5536', system: 'Nintendo 64' }, // War Gods
+		{ id: '59511', system: 'Nintendo 64' }, // WinBack : Covert Operations
+		{ id: '5518', system: 'Nintendo 64' } // Xena : Warrior Princess - The Talisman of Fate
+	]
+};
---
+++ filters/rg351p/psp-compat.js
@@ -0,0 +1,101 @@
+/*
+	A basic list of PSP games confirmed compatible with rg351p.
+	Sourced from https://github.com/jserodio/rg351p-ppsspp-settings/wiki.
+	Not exhaustive list and open to additions based off feedback.
+*/
+
+module.exports = {
+	include: [
+		{ id: '28282', system: 'Sony Playstation Portable' }, // Ape Escape : On the Loose
+		{ id: '27858', system: 'Sony Playstation Portable' }, // Assassin's Creed : Bloodlines
+		{ id: '28962', system: 'Sony Playstation Portable' }, // Bejeweled 2
+		{ id: '28155', system: 'Sony Playstation Portable' }, // BlazBlue : Continuum Shift II
+		{ id: '28389', system: 'Sony Playstation Portable' }, // Bleach : Heat the Soul 4
+		{ id: '28178', system: 'Sony Playstation Portable' }, // Brave Story : New Traveler
+		{ id: '28278', system: 'Sony Playstation Portable' }, // Bomberman
+		{ id: '28184', system: 'Sony Playstation Portable' }, // Cars
+		{ id: '28043', system: 'Sony Playstation Portable' }, // Cars 2
+		{ id: '27873', system: 'Sony Playstation Portable' }, // Castlevania : The Dracula X Chronicles
+		{ id: '28594', system: 'Sony Playstation Portable' }, // ClaDun : This is an RPG
+		{ id: '28762', system: 'Sony Playstation Portable' }, // Cladun X2
+		{ id: '28417', system: 'Sony Playstation Portable' }, // Coded Arms
+		{ id: '28404', system: 'Sony Playstation Portable' }, // Crush
+		{ id: '140700', system: 'Sony Playstation Minis' }, // Days Of Thunder
+		{ id: '27938', system: 'Sony Playstation Portable' }, // Daxter
+		{ id: '28509', system: 'Sony Playstation Portable' }, // Dave Mirra BMX Challenge
+		{ id: '28443', system: 'Sony Playstation Portable' }, // Dead to Rights : Reckoning
+		{ id: '27923', system: 'Sony Playstation Portable' }, // Def Jam : Fight for NY, The Takeover
+		{ id: '28176', system: 'Sony Playstation Portable' }, // Disgaea Infinite
+		{ id: '27896', system: 'Sony Playstation Portable' }, // Disgaea 2 : Dark Hero Days
+		{ id: '27841', system: 'Sony Playstation Portable' }, // Dissidia : Final Fantasy
+		{ id: '27830', system: 'Sony Playstation Portable' }, // Dissidia 012 : Duodecim Final Fantasy
+		{ id: '27853', system: 'Sony Playstation Portable' }, // Dragon Ball Z : Shin Budokai Another Road
+		{ id: '28259', system: 'Sony Playstation Portable' }, // Exit
+		{ id: '28274', system: 'Sony Playstation Portable' }, // Exit 2
+		{ id: '28086', system: 'Sony Playstation Portable' }, // Dynasty Warriors : Strikeforce
+		{ id: '102316', system: 'Sony Playstation Portable' }, // Family Guy
+		{ id: '28550', system: 'Sony Playstation Portable' }, // Fate-Tiger Colosseum
+		{ id: '27864', system: 'Sony Playstation Portable' }, // Final Fantasy Tactics : The War of the Lions
+		{ id: '27848', system: 'Sony Playstation Portable' }, // Final Fantasy III
+		{ id: '27836', system: 'Sony Playstation Portable' }, // Final Fantasy VII : Crisis Core
+		{ id: '28506', system: 'Sony Playstation Portable' }, // Full Auto 2 : Battlelines
+		{ id: '27965', system: 'Sony Playstation Portable' }, // Gods Eater Burst
+		{ id: '27865', system: 'Sony Playstation Portable' }, // Grand Theft Auto : Chinatown Wars
+		{ id: '28432', system: 'Sony Playstation Portable' }, // Gurumin : A Monstrous Adventure
+		{ id: '28735', system: 'Sony Playstation Portable' }, // Gunpey
+		{ id: '28248', system: 'Sony Playstation Portable' }, // Half-Minute Hero
+		{ id: '28350', system: 'Sony Playstation Portable' }, // Hexyz Force
+		{ id: '28706', system: 'Sony Playstation Portable' }, // Holy Invasion of Privacy, Badman! What Did I Do To Deserve This?
+		{ id: '144268', system: 'Sony Playstation Portable' }, // Hot Shots Tennis : Get a Grip
+		{ id: '28510', system: 'Sony Playstation Portable' }, // Initial D : Street Stage
+		{ id: '144336', system: 'Sony Playstation Portable' }, // 	Kamen no Maid Guy : Boyoyon Battle Royale
+		{ id: '27815', system: 'Sony Playstation Portable' }, // Kingdom Hearts : Birth by Sleep
+		{ id: '27881', system: 'Sony Playstation Portable' }, // LittleBigPlanet
+		{ id: '28125', system: 'Sony Playstation Portable' }, // LocoRoco
+		{ id: '28269', system: 'Sony Playstation Portable' }, // Lunar : Silver Star Harmony
+		{ id: '27991', system: 'Sony Playstation Portable' }, // Mega Man : Maverick Hunter X
+		{ id: '27820', system: 'Sony Playstation Portable' }, // Monster Hunter Freedom Unite
+		{ id: '28013', system: 'Sony Playstation Portable' }, // Mortal Kombat : Unchained
+		{ id: '28556', system: 'Sony Playstation Portable' }, // Nayuta no Kiseki
+		{ id: '28012', system: 'Sony Playstation Portable' }, // Need for Speed : Underground Rivals
+		{ id: '27958', system: 'Sony Playstation Portable' }, // Need for Speed : Most Wanted 5-1-0
+		{ id: '27977', system: 'Sony Playstation Portable' }, // Obscure : The Aftermath
+		{ id: '28061', system: 'Sony Playstation Portable' }, // Patapon
+		{ id: '28024', system: 'Sony Playstation Portable' }, // Shin Megami Tensei : Persona
+		{ id: '27917', system: 'Sony Playstation Portable' }, // Shin Megami Tensei : Persona 2, Innocent Sin
+		{ id: '27886', system: 'Sony Playstation Portable' }, // Shin Megami Tensei : Persona 3 Portable
+		{ id: '28723', system: 'Sony Playstation Portable' }, // Pipe Mania
+		{ id: '29331', system: 'Sony Playstation Minis' }, // Pix'n Love Rush
+		{ id: '142882', system: 'Sony Playstation Portable' }, // Prince Of Persia : The Forgotten Sands
+		{ id: '28304', system: 'Sony Playstation Portable' }, // Prinny : Can I Really Be the Hero
+		{ id: '144638', system: 'Sony Playstation Portable' }, // Pro Bull Riders: Out of the Chute
+		{ id: '28438', system: 'Sony Playstation Portable' }, // Rengoku II : The Stairway to H.E.A.V.E.N.
+		{ id: '28136', system: 'Sony Playstation Portable' }, // Ridge Racer
+		{ id: '28439', system: 'Sony Playstation Portable' }, // Ridge Racer 2
+		{ id: '28245', system: 'Sony Playstation Portable' }, // Rocky Balboa
+		{ id: '144860', system: 'Sony Playstation Portable' }, // SBK Superbike World Championship
+		{ id: '28273', system: 'Sony Playstation Portable' }, // Shinobido : Tales of the Ninja
+		{ id: '27937', system: 'Sony Playstation Portable' }, // Star Ocean : First Departure
+		{ id: '28096', system: 'Sony Playstation Portable' }, // Steins : Gate
+		{ id: '28413', system: 'Sony Playstation Minis' }, // Sky Force
+		{ id: '27956', system: 'Sony Playstation Portable' }, // Soulcalibur : Broken Destiny
+		{ id: '27874', system: 'Sony Playstation Portable' }, // Tactics Ogre : Let Us Cling Together
+		{ id: '27975', system: 'Sony Playstation Portable' }, // Tekken : Dark Resurrection
+		{ id: '28296', system: 'Sony Playstation Minis' }, // Tetris
+		{ id: '142891', system: 'Sony Playstation Portable' }, // The Con
+		{ id: '27860', system: 'Sony Playstation Portable' }, // The 3rd Birthday
+		{ id: '103341', system: 'Sony Playstation Portable' }, // The Legend of Heroes : Trails in the Sky
+		{ id: '142931', system: 'Sony Playstation Portable' }, // The Sims 2
+		{ id: '28075', system: 'Sony Playstation Portable' }, // Ultimate Ghosts 'n Goblins
+		{ id: '28293', system: 'Sony Playstation Portable' }, // Hot Shots Golf : Open Tee
+		{ id: '144885', system: 'Sony Playstation Portable' }, // Valkyria Chronicles II
+		{ id: '28210', system: 'Sony Playstation Portable' }, // Virtua Tennis : World Tour
+		{ id: '29005', system: 'Sony Playstation Portable' }, // Wangan Midnight Portable
+		{ id: '28231', system: 'Sony Playstation Portable' }, // Wild Arms XF
+		{ id: '28106', system: 'Sony Playstation Portable' }, // Ys : The Oath in Felghana
+		{ id: '27959', system: 'Sony Playstation Portable' }, // Ys Seven
+		{ id: '28433', system: 'Sony Playstation Portable' }, // Ys vs. Sora no Kiseki : Alternative Saga
+		{ id: '28427', system: 'Sony Playstation Minis' }, // Zombie Tycoon
+		{ id: '28179', system: 'Sony Playstation Portable' } // Z.H.P. : Unlosing Ranger vs. Darkdeath Evilman
+	]
+};
