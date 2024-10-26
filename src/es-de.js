647e5b635a7f1f9f7bb0cd141eb16582d5cb20da
JayCanuck <robitaille88@gmail.com>
2024-10-25 17:25:57 -0700

    Support link/move/copy es-de actions

--- src/es-de.js
+++ src/es-de.js
@@ -6,28 +6,33 @@ const fs = require('fs-extra');
 
 const options = {
 	boolean: ['quiet', 'help'],
-	alias: { m: 'multi', q: 'quiet', h: 'help' }
+	string: ['action', 'config'],
+	default: { action: 'link', config: path.join(os.homedir(), '.emulationstation') },
+	alias: { a: 'action', c: 'config', m: 'multi', q: 'quiet', h: 'help' }
 }; // multi omitted as it can be string or boolean
 
-const help = function () {
+const help = function (exitCode = 0) {
 	console.log('  Usage');
 	console.log('    gamelist es-de [options]');
 	console.log();
 	console.log('  Options');
-	console.log('    -c, --copy-xml    Copy gamelist.xml rather than symlink');
+	console.log('    -a, --action      Action to apply to gamelist.xml and assets');
+	console.log('                          One of: link, move, copy');
+	console.log('                          Default to "link"');
+	console.log('    -c, --config      ES-DE config directory path');
+	console.log('                          Default to "~/.emulationstation"');
 	console.log('    -q, --quiet       Quiet console output');
 	console.log('    -m, --multi       Target subdirectories instead of working directory');
 	console.log('                          Comma-separated list or "all" for all');
 	console.log('    -v, --version     Display version information');
 	console.log('    -h, --help        Display help information');
 	console.log();
-	process.exit(0);
+	process.exit(exitCode);
 };
 
-const api = async function (dir, { 'copy-xml': copyXml, quiet } = {}) {
-	const esUserDir = path.resolve(
-		process.env.ESDE_USERDIR || path.join(os.homedir(), '.emulationstation')
-	);
+const api = async function (dir, { action, config, quiet } = {}) {
+	const esConfigDir = path.resolve(config);
+	const esConfigName = path.basename(config);
 	const esMedia = process.env.ESDE_MEDIA || 'downloaded_media';
 	const media = process.env.GAMELIST_MEDIA || 'media';
 	const mediaPairs = [
@@ -37,49 +42,102 @@ const api = async function (dir, { 'copy-xml': copyXml, quiet } = {}) {
 		['screenshots', process.env.GAMELIST_SCREENSHOT || 'screenshot'],
 		['videos', process.env.GAMELIST_SNAP || 'snap'],
 		['titlescreens', process.env.GAMELIST_TITLE || 'title'],
-		['marquees', process.env.GAMELIST_MARQUEE || 'wheel']
+		['marquees', process.env.GAMELIST_MARQUEE || 'wheel'],
+		['manuals', process.env.GAMELIST_MANUAL || 'manual']
 	];
-	if (fs.existsSync(esUserDir)) {
-		const xml = path.join(dir, 'gamelist.xml');
-		if (fs.existsSync(xml)) {
-			const system = path.basename(dir);
-			const esGamelistDir = path.join(esUserDir, 'gamelists', system);
-			const linkXml = path.join(esGamelistDir, 'gamelist.xml');
-			const mediaDir = path.join(dir, media);
 
+	fs.ensureDirSync(esConfigDir);
+	const xml = path.join(dir, 'gamelist.xml');
+	const system = path.basename(dir);
+	const mediaDir = path.join(dir, media);
+
+	switch (action) {
+		case 'link':
 			if (!quiet) console.log(`Linking ${system} metadata`);
-			fs.ensureDirSync(esGamelistDir);
-			fs.removeSync(linkXml);
-			if (copyXml) {
-				fs.copySync(xml, linkXml);
-				if (!quiet)
-					console.log(
-						`\t${system}/gamelist.xml >> ~/.emulationstation/gamelists/${system}/gamelist.xml`
-					);
-			} else {
+			break;
+		case 'move':
+			if (!quiet) console.log(`Moving ${system} metadata`);
+			break;
+		case 'copy':
+			if (!quiet) console.log(`Copying ${system} metadata`);
+			break;
+		default:
+			console.error('Invalid action type\n');
+			help(1);
+	}
+
+	// handle gamelist.xml
+	if (fs.existsSync(xml)) {
+		const esGamelistDir = path.join(esConfigDir, 'gamelists', system);
+		const linkXml = path.join(esGamelistDir, 'gamelist.xml');
+
+		fs.ensureDirSync(esGamelistDir);
+
+		fs.removeSync(linkXml);
+		switch (action) {
+			case 'link':
 				fs.symlinkSync(xml, linkXml);
 				if (!quiet)
 					console.log(
-						`\t${system}/gamelist.xml <==> ~/.emulationstation/gamelists/${system}/gamelist.xml`
+						`\t${system}/gamelist.xml <==> ${esConfigName}/gamelists/${system}/gamelist.xml`
 					);
-			}
-			if (fs.existsSync(mediaDir)) {
-				const esDownloadedMediaDir = path.join(esUserDir, esMedia, system);
-				fs.ensureDirSync(esDownloadedMediaDir);
-				mediaPairs.forEach(([esMediaTypeDir, mediaTypeDir]) => {
-					const resolvedEsMediaTypeDir = path.join(esDownloadedMediaDir, esMediaTypeDir);
-					const resolvedMediaTypeDir = path.join(mediaDir, mediaTypeDir);
-					fs.removeSync(resolvedEsMediaTypeDir);
-					fs.symlinkSync(resolvedMediaTypeDir, resolvedEsMediaTypeDir);
-					if (!quiet)
-						console.log(
-							`\t${system}/media/${mediaTypeDir} <==> ~/.emulationstation/${esMedia}/${system}/${esMediaTypeDir}`
-						);
-				});
-			}
+				break;
+			case 'move':
+				fs.renameSync(xml, linkXml);
+				if (!quiet)
+					console.log(
+						`\t${system}/gamelist.xml > ${esConfigName}/gamelists/${system}/gamelist.xml`
+					);
+				break;
+			case 'copy':
+				fs.copySync(xml, linkXml);
+				if (!quiet)
+					console.log(
+						`\t${system}/gamelist.xml >> ${esConfigName}/gamelists/${system}/gamelist.xml`
+					);
 		}
-	} else {
-		throw new Error(`EmulationStation-DE home config directory not found: ${esUserDir}`);
+	}
+
+	// handle media files
+	if (fs.existsSync(mediaDir)) {
+		const esDownloadedMediaDir = path.join(esConfigDir, esMedia, system);
+		fs.ensureDirSync(esDownloadedMediaDir);
+		mediaPairs.forEach(([esMediaTypeDir, mediaTypeDir]) => {
+			const resolvedEsMediaTypeDir = path.join(esDownloadedMediaDir, esMediaTypeDir);
+			const resolvedMediaTypeDir = path.join(mediaDir, mediaTypeDir);
+
+			// handle media type
+			if (fs.existsSync(resolvedMediaTypeDir)) {
+				fs.removeSync(resolvedEsMediaTypeDir);
+				switch (action) {
+					case 'link':
+						fs.symlinkSync(resolvedMediaTypeDir, resolvedEsMediaTypeDir);
+						if (!quiet)
+							console.log(
+								`\t${system}/media/${mediaTypeDir} <==> ${esConfigName}/${esMedia}/${system}/${esMediaTypeDir}`
+							);
+						break;
+					case 'move':
+						fs.renameSync(resolvedMediaTypeDir, resolvedEsMediaTypeDir);
+						if (!quiet)
+							console.log(
+								`\t${system}/media/${mediaTypeDir} > ${esConfigName}/${esMedia}/${system}/${esMediaTypeDir}`
+							);
+						break;
+					case 'copy':
+						fs.ensureDirSync(resolvedEsMediaTypeDir);
+						fs.copySync(resolvedMediaTypeDir, resolvedEsMediaTypeDir);
+						if (!quiet)
+							console.log(
+								`\t${system}/media/${mediaTypeDir} >> ${esConfigName}/${esMedia}/${system}/${esMediaTypeDir}`
+							);
+				}
+			}
+		});
+
+		if (fs.readdirSync(mediaDir).length === 0) {
+			fs.removeSync(mediaDir);
+		}
 	}
 };
 
